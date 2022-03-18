9b9e1a37f92e2c980e9d13f44ef72b8b3ebba838
Natasha Dronova <natalia.dronova@aginic.com>
2022-03-18 11:23:47 +1100

    bla

--- models/transaction.sql
+++ models/transaction.sql
@@ -1,7 +1,29 @@
-SELECT 
-    * 
-    ,row_number() over (
-    partition by claim_transaction_key
-    order by updatetime
-) as version_no
-FROM {{ ref('intg_claim_transaction') }}
+{{
+    config(
+        materialized='incremental',
+        unique_key='sk'
+    )
+}}
+
+
+WITH source_a AS (
+  SELECT *,
+  concat(id,dbt_valid_from) as sk
+ FROM {{ ref('source_cc_transaction_line_src_a') }}
+    {% if is_incremental() %}
+    where updatetime >= (select max(updatetime)::timestamp from {{this}})
+  {% endif %}
+),
+
+source_b AS (
+  SELECT *,
+  concat(id,dbt_valid_from) as sk
+FROM {{ ref('source_cc_transaction_line_src_b') }}
+ {% if is_incremental() %}
+    where updatetime > (select max(updatetime)::timestamp from {{this}})
+  {% endif %}
+)
+
+SELECT * FROM source_a
+UNION 
+SELECT * FROM source_b
